e2450c03b6980f510ceeb8ad641665337b5edf25
asaelmans <156896035+asaelmans@users.noreply.github.com>
2025-06-30 11:14:43 +0200

    Update helpers.R

--- R/helpers.R
+++ R/helpers.R
@@ -1,1 +1,8 @@
-
+#' @export
+loadStudySpec <- function(type = 'validation.json'){
+  analysisSpecifications <- ParallelLogger::loadSettingsFromJson(
+    fileName = system.file('study_execution_jsons',type,
+                           package = 'QRISKvalidation')
+    )
+  return(analysisSpecifications)
+}
