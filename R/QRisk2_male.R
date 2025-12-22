f82b9d715d020bab9bb541ad080f8e9851ac8fe0
asaelmans <156896035+asaelmans@users.noreply.github.com>
2025-12-22 17:02:10 +0100

    Update QRisk2_male.R

--- R/QRisk2_male.R
+++ R/QRisk2_male.R
@@ -245,40 +245,131 @@ baseline*exp(x)
     ),
 
     # white or not recorded 22466652
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 652, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(22466), 
+        cohortName = c('White or not recorded')
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
       
-    22466, # White or not recorded
 
     # indian 21377662
-      
-    21377, # Indian
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 662, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(21377), 
+        cohortName = c('Indian')
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
 
     #Pakistani 21378672
-      
-    21378, # Pakistani
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 672, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(21378), 
+        cohortName = c('Pakistani')
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
 
     #Bangladeshi 21379682
-      
-    21379, # Bangladeshi
-
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 682, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(21379), 
+        cohortName = c('Bangladeshi')
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
+   
     #Other Asian 21380692
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 692, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(21380), 
+        cohortName = c('Other Asian')
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
     
-    21380, # Other Asian
-
     #Black Caribbean 21381702
-      
-    21381, # Black Caribbean
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 702, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(21381), 
+        cohortName = c('Black Caribbean')
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
 
     #Black African 21382712
-      
-    21382, # Black African
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 712, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(21382), 
+        cohortName = c('Black African')
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
 
     #Chinese 21383722
-      
-    21383, # Chinese
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 722, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(21383), 
+        cohortName = c('Chinese')
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
 
     #Other 21386732
-      
-    21386, # Other
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 732, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(21386), 
+        cohortName = c('Other')
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
 
     # Age x BMI Interaction
 
