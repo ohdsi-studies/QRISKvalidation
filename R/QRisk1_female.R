1129c9128da7a047d045ba32c0cb11aadadb4b2f
asaelmans <156896035+asaelmans@users.noreply.github.com>
2025-12-22 11:24:36 +0100

    Update QRisk1_female.R

--- R/QRisk1_female.R
+++ R/QRisk1_female.R
@@ -50,7 +50,7 @@ plpModelQRISK1_female_OG_10 <- PatientLevelPrediction::createGlmModel(
   ), 
   intercept = 0, 
   mapping = "function(x){ sapply(x, function(x){
-baseline <- 0.0009293994
+baseline <- 0.07464244
 baseline*exp(x)
 })}", 
   populationSettings = PatientLevelPrediction::createStudyPopulationSettings(
