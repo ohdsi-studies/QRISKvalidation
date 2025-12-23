4dfdbdc136f976dcac52877f04be43c76d3f1011
asaelmans <156896035+asaelmans@users.noreply.github.com>
2025-12-23 13:02:23 +0100

    Update QRisk4_female.R

--- R/QRisk4_female.R
+++ R/QRisk4_female.R
@@ -1,1 +1,310 @@
+#' Create qrisk 4 model for males using Qrisk cohorts
+#'
+#' @details
+#' The user specifies 
+#'
+#' @param cohortDatabaseSchema the schema where the cohorts are generated into
+#' @param cohortTableName the cohort table name
+#' @param sampleSize the sample size to take of the target cohort if it is too large
+#'
+#' @return
+#' An plpModel
+#' 
+#' @export
+createQRISK4MaleOG10 <- function(
+    cohortDatabaseSchema,
+    cohortTableName = 'qrisk_cprd',
+    sampleSize = 10000
+  ){
+  
+  # Male qrisk4
+    
+plpModelQRISK4_male_OG_10 <- PatientLevelPrediction::createGlmModel(
+  targetId = 7, # the first cohort I create manually
+  outcomeId = 21397, 
+  restrictPlpDataSettings = PatientLevelPrediction::createRestrictPlpDataSettings(
+    sampleSize = sampleSize # sampling for testing
+    ),# can use this to restrict dates as well
+  coefficients = data.frame(
+    covariateId = c(
+        3466, # Systolic blood pressure
+        3467, # SD of blood pressure                     
+        1486, # Cholesterol/HDL
+        4466, # Deprivation (Townsend)
+        
+        21288655, # Former smoker
+        21289655, # Light smoker
+        21290655, # Moderate smoker
+        21291655, # Heavy smoker
+        
+        22466652, # White or not recorded               
+        21377652, # Indian
+        21378652, # Pakistani
+        21379652, # Bangladeshi
+        21380652, # Other Asian
+        21381652, # Black Caribbean
+        21382652, # Black African
+        21383652, # Chinese
+        21386652, # Other  
+        
+        22465668, # No learning disability
+        21344668, # Learning disability - Jenna note: is this recorded?
+        19881668, # Down syndrome
+        
+        18821668, # Family history of coronary heart disease
+        18820668, # Type I Diabetes
+        18815668, # Type II Diabetes
+        19280688, # Treated hypertension - recent 
+        18838668, # Rheumatoid arthritis
+        18841668, # Atrial fibrillation
+        21347668, # Renal disease
+        19379668, # Migraine
+        21371668, # Corticosteroid use
+        19164668, # Systemic lupus erythematosus
+        21372668, # Atypical antipsychotic use
+        21294668, # Severe mental illness
+        19380668, # COPD
+        19792668, # Lung cancer
+        19788668, # Oral cancer
+        19787668, # Blood cancer
+        19174668, # Brain cancer
+        19793668, # Post natal depression
+        14512668 # Pre eclampsia
+    ),       
+    coefficient = c(0.0635, 1.16, 1.14, 0.252, 1.19, 2.12, 2.30, 2.90, 1.00, 1.13, 1.56, 1.28, 0.98, 0.94, 0.76, 0.68, 0.94, 1.00, 1.45, 3.18, 1.46, 4.52, 2.49, 2.20, 1.28, 4.50, 1.81, 1.46, 1.70, 2.22, 1.24, 1.22, 1.85, 3.50, 1.55, 2.13, 4.52, 1.18, 1.56) 
+  ), 
+  intercept = 0, 
+  mapping = "function(x){ sapply(x, function(x){
+baseline <- 0.03550895
+baseline*exp(x)
+})}", 
+  populationSettings = PatientLevelPrediction::createStudyPopulationSettings(
+    requireTimeAtRisk = FALSE, 
+    riskWindowStart = 1, 
+    startAnchor = 'cohort start',
+    riskWindowEnd = 3650, 
+    endAnchor = 'cohort start'
+  ),
+  covariateSettings = list(
+    # creates a Chol/HDL ratio covariate with id 1466
+    QRISKvalidation::createMeasurementRatioCovariateSettings(
+      covariateName = 'Chol/HDL ratio', 
+      conceptSet1 = c(4260765), # first measurement
+      conceptSet2 = c(4042059,4076704), # second measurement 
+      unitSet1 = NULL, # may need to edit this if units can be different across network
+      unitSet2 = NULL, # may need to edit this if units can be different across network
+      startDay = -365*3, # last three years - TODO edit this
+      endDay = 0, 
+      centeringMap = function(covariates){
+        covariates$covariateValue <- sapply(covariates$covariateValue, function(y){y - 4})
+        return(covariates)
+      }, 
+      minVal1 = 0,
+      maxVal1 = 5000, # is there a max value that past this must be an error?
+      minVal2 =  0,
+      maxVal2 = NULL, # is there a max value that past this must be an error?
+      aggregateMethod = 'recent',
+      covariateId = 1486, 
+      analysisId = 486
+    ),
+    
+    # townsend measurement covariate 4466
+    QRISKvalidation::createMeasurementCovariateSettings(
+      covariateName = 'Townsend', 
+      conceptSet = c(715996),
+      unitSet = NULL, 
+      startDay = -9999, 
+      endDay = 0, 
+      scaleMap = function(covariates){
+        covariates$valueAsNumber <- sapply(covariates$valueAsNumber, function(y){y - 0})
+        return(covariates)
+      }, 
+      minVal = NULL,
+      maxVal = NULL, 
+      aggregateMethod = 'recent',
+      covariateId = 4466, 
+      analysisId = 466
+    ),
+    
+    # Systolic blood pressure covariateId 3466
+    QRISKvalidation::createMeasurementCovariateSettings(
+      covariateName = 'Systolic Blood Pressure', 
+      conceptSet = c(3004249),
+      unitSet = NULL, 
+      startDay = -365, 
+      endDay = 0, 
+      scaleMap = function(covariates){
+        covariates$valueAsNumber <- sapply(covariates$valueAsNumber, function(y){y - 132.6})
+        return(covariates)
+      }, 
+      minVal = 5,
+      maxVal = 250, 
+      aggregateMethod = 'recent',
+      covariateId = 3466, 
+      analysisId = 466
+    ),
+    
+    QRISKvalidation::createMeasurementCovariateSettings(
+      covariateName = 'Systolic Blood Pressure', 
+      conceptSet = c(3004249),
+      unitSet = NULL, 
+      startDay = -365, 
+      endDay = 0, 
+      scaleMap = function(covariates){
+        covariates$valueAsNumber <- sapply(covariates$valueAsNumber, function(y){y - 132.6})
+        return(covariates)
+      }, 
+      minVal = 5,
+      maxVal = 250, 
+      aggregateMethod = 'stdev', 
+      covariateId = 3467, 
+      analysisId = 467
+    ),
+    
+    # treatment for blood pressure 19280688
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 688, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(19280), 
+        cohortName = c('Antihypertensive agent')
+      ), 
+      valueType = 'binary', 
+      startDay = -30, 
+      endDay = 0
+    ),
+    
+    # family history of CVD covariate 18821668
+    # type 2 diabetes 18815668
+    # rheumatoid arthritis 18838668
+    # atrial fibrillation 18841668
+    # renal disease 21347668
+    # Type I diabetes 18820668
+    # Migraine 19379668
+    # Corticosteroid use 21371668
+    # SLE 19164668
+    # Atypical antipsychotic use 21372668
+    # Severe mental illness 21294668
+    # Pre eclampsia 14512668
+    # Postnatal depression 19793668
+    # No learning disability 22465668
+    # Learning disability 21344668
+    # Down syndrome 19881668
+    # COPD 19380668
+    # Lung cancer 19792668
+    # Oral cancer 19788668
+    # Blood cancer 19787668
+    # Brain cancer 19174668
+    
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 668, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(18821, 18815, 18838, 18841,
+                     21347, 18820, 19379, 21371,
+                     19164, 21372, 21294, 14512
+                     19793, 22465, 21344, 19881, 
+                     19380, 19792, 19788, 19787, 
+                     19174), 
+        cohortName = c('Family history of premature cardiovascular disease',
+                       'Type 2 diabetes',
+                       'Rheumatoid arthritis',
+                       'Atrial fibrillation',
+                       'Renal disease',
+                       'Type I Diabetes',
+                       'Migraine',
+                       'Corticosteroids',
+                       'SLE',
+                       'Atypical antipsychotic use',
+                       'Severe mental illness',
+                       'Pre eclampsia',
+                       'Postnatal depression',
+                       'No learning disability',
+                       'Learning disability',
+                       'Down syndrome',
+                       'COPD',
+                       'Lung cancer',
+                       'Oral cancer',
+                       'Blood cancer',
+                       'Brain cancer'
+                       )
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
 
+
+    # current smoker - covariateId 19285678
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 678, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(19285), 
+        cohortName = c('Current smoker')
+      ), 
+      valueType = 'binary', 
+      startDay = -365, 
+      endDay = 0
+    ),
+    # white or not recorded 22466652
+    # indian 21377652
+    # Pakistani 21378652
+    # Bangladeshi 21379652
+    # Other Asian 21380652
+    # Black Caribbean 21381652
+    # Black African 21382652
+    # Chinese 21383652
+    # Other 21386652
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 652, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(22466, 21377, 21378,
+                     21379, 21380, 21381,
+                     21382, 21383, 21386), 
+        cohortName = c('White or not recorded',
+                       'Indian', 'Pakistani',
+                       'Bangladeshi',
+                       'Other Asian',
+                       'Black Caribbean',
+                       'Black African',
+                       'Chinese',
+                       'Other')
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
+    
+    
+    # Non smoker 21287655
+    # Former smoker 21288655
+    # Light smoker 21289655
+    # Moderate smoker 21290655
+    # Heavy smoker 21291655
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 655, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(21287, 21288, 21289,
+                     21290, 21291), 
+        cohortName = c('Non smoker', 'Former smoker',
+                       'Light smoker', 'Moderate smoker',
+                       'Heavy smoker')
+      ), 
+      valueType = 'binary', 
+      startDay = -365, 
+      endDay = 0
+    )
+    
+  )                 
+)
+
+return(plpModelQRISK4_male_OG_10)
+}
