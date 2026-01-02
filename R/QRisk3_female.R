a79c3f959214ffce0f6f5350195d4cf3bd6f4820
asaelmans <156896035+asaelmans@users.noreply.github.com>
2026-01-02 11:16:23 +0100

    Update QRisk3_female.R
    Creating QRISK 3 female with QRISK 3 male as example

--- R/QRisk3_female.R
+++ R/QRisk3_female.R
@@ -29,15 +29,19 @@ plpModelQRISK3_female_OG_10 <- PatientLevelPrediction::createGlmModel(
     covariateId = c(
         4466, # Townsend
         
-        22466652, # White or not recorded               
-        21377652, # Indian
-        21378652, # Pakistani
-        21379652, # Bangladeshi
-        21380652, # Other Asian
-        21381652, # Black Caribbean
-        21382652, # Black African
-        21383652, # Chinese
-        21386652, # Other
+        8527004, # White or not recorded 
+        38003614004, # white
+        38003574004, # Indian
+        38003589004, # Pakistani
+        38003575004, # Bangladeshi
+        8515004, # Other Asian
+        38003590004, # Other Asian
+        38003581004, # Other Asian 
+        38003609004, # Black Caribbean
+        38003600004, # Black African
+        8516004, # Black African
+        38003579004, # Chinese
+        38003615004, # Other
         
         21287655, # Non smoker
         21288655, # Former smoker
@@ -45,30 +49,151 @@ plpModelQRISK3_female_OG_10 <- PatientLevelPrediction::createGlmModel(
         21290655, # Moderate smoker
         21291655, # Heavy smoker 
         
-        18821668, # Family history of coronary heart disease
+        
+        18841668, # Atrial fibrillation
+        21372668, # Atypical antipsychotic use
+        21371668, # Corticosteroid use
+        19379668, # Migraine
+        18838668, # Rheumatoid arthritis
+        21347668, # Renal disease
+        21294668, # Severe mental illness
+        19164668, # Systemic lupus erythematosus
+        19280688, # Treated hypertension
         18820668, # Type I Diabetes
         18815668, # Type II Diabetes
-        19280688, # Treated hypertension
-        18838668, # Rheumatoid arthritis
-        18841668, # Atrial fibrillation
-        21347668, # Renal disease
-        19379668, # Migraine
-        21371668, # Corticosteroid use
-        19164668, # Systemic lupus erythematosus
-        21372668, # Atypical antipsychotic use
-        21294668, # Severe mental illness
+        18821668, # Family history of coronary heart disease
         
-        1486, # Cholesterol/HDL
+        1020, #age1
+        2020, #age2
+        1021, #bmi 1
+        2021, #bmi 2
+        1466, # Cholesterol/HDL
         3466, # Systolic blood pressure
-        3467 # SD of blood pressure                                                           
+        3495, # SD of blood pressure 5-year
+        
+        # age1 interaction
+        21288655123, # Former smoker
+        21289655123, # Light smoker
+        21290655123, # Moderate smoker
+        21291655123, # Heavy smoker 
+        18841668123, # Atrial fibrillation
+        #21372668123, # Atypical antipsychotic use
+        21371668123, # Corticosteroid use
+        19379668123, # Migraine
+        #18838668, # Rheumatoid arthritis
+        21347668123, # Renal disease
+        #21294668, # Severe mental illness
+        #19164668, # Systemic lupus erythematosus
+        19280688123, # TODO- Treated hypertension
+        18820668123, # Type I Diabetes
+        18815668123, # Type II Diabetes
+        1021123,  # bmi 1
+        2021123,  # bmi 2
+        18821668123, # Family history of coronary heart disease
+        3466123, #sbp
+        4466123, #townsend
+        
+
+        # age2 interaction
+        21288655124, # Former smoker
+        21289655124, # Light smoker
+        21290655124, # Moderate smoker
+        21291655124, # Heavy smoker 
+        18841668124, # Atrial fibrillation
+        #21372668124, # Atypical antipsychotic use
+        21371668124, # Corticosteroid use
+        19379668124, # Migraine
+        #18838668, # Rheumatoid arthritis
+        21347668124, # Renal disease
+        #21294668, # Severe mental illness
+        #19164668, # Systemic lupus erythematosus
+        19280688124, # TODO- Treated hypertension
+        18820668124, # Type I Diabetes
+        18815668124, # Type II Diabetes
+        1021124,  # bmi 1
+        2021124,  # bmi 2
+        18821668124, # Family history of coronary heart disease
+        3466124, #sbp
+        4466124 #townsend
     ), 
-    coefficient = c(0.294, 1.00, 1.32, 1.76, 1.34, 1.08, 0.843, 0.675, 0.722, 0.843, 1.00, 1.14, 1.75, 1.95, 2.34, 1.58, 5.62, 2.91, 1.66, 1.24, 4.92, 1.92, 1.35, 1.81, 2.14, 1.29, 1.13, 1.17, 0.057, 0.108
-                   )
+    coefficient = c(
+      # townsend
+      0.0332682012772872950000000,
+      
+      # race/ethnicity
+      0, 0, 0.2771924876030827900000000, 0.4744636071493126800000000, 0.5296172991968937100000000,
+      0.0351001591862990170000000, 0.0351001591862990170000000,0.0351001591862990170000000,
+      -0.3580789966932791900000000, -0.4005648523216514000000000, -0.4005648523216514000000000,
+      -0.4152279288983017300000000, -0.2632134813474996700000000,
+      
+      # smoker low to high
+      0, 0.1912822286338898300000000, 0.5524158819264555200000000, 0.6383505302750607200000000,
+      0.7898381988185801900000000,
+      
+      # binary
+      0.8820923692805465700000000,
+      0.1304687985517351300000000,
+      0.4548539975044554300000000,
+      0.2558417807415991300000000,
+      0.2097065801395656700000000,
+      0.7185326128827438400000000,
+      0.1213303988204716400000000,
+      0.4401572174457522000000000,
+      0.5165987108269547400000000,
+      1.2343425521675175000000000,
+      0.8594207143093222100000000,
+      0.5405546900939015600000000,
+      
+      # measurements
+      -17.8397816660055750000000000,
+      0.0022964880605765492000000,
+      2.4562776660536358000000000,
+      -8.3011122314711354000000000,
+      0.1734019685632711100000000,
+      0.0129101265425533050000000,
+      0.0102519142912904560000000,
+      
+      # interactions - age 1
+      
+      -0.2101113393351634600000000, # Former smoker
+      0.7526867644750319100000000,
+      0.9931588755640579100000000,
+      2.1331163414389076000000000,
+      3.4896675530623207000000000, #afib
+      1.1708133653489108000000000, #steroids
+      2.3491159871402441000000000, #migraine
+      -0.5065671632722369400000000, # renal
+      6.5114581098532671000000000, #treated hyper
+      5.3379864878006531000000000, # type 1
+      3.6461817406221311000000000, # type 2
+      31.0049529560338860000000000, #bmi 1
+      -111.2915718439164300000000000, #bmi2
+      2.7808628508531887000000000, #fhc
+      0.0188585244698658530000000, #sbp
+      -0.1007554870063731000000000, #townsend
+      
+      # interactions - age 2
+      -0.0004985487027532612100000, # smoke
+      -0.0007987563331738541400000, # smoke
+      -0.0008370618426625129600000, # smoke
+      -0.0007840031915563728900000, # smoke
+      -0.0003499560834063604900000, #afib
+      -0.0002496045095297166000000, #steriods
+      0.0001989644604147863100000, #migraine
+      -0.0018325930166498813000000, #renale
+      0.0006383805310416501300000, #treated hypertension
+      0.0006409780808752897000000, # type 1
+      -0.0002469569558886831500000, #type 2
+      0.0050380102356322029000000, #bmi 1
+      -0.0130744830025243190000000, #bmi 2
+      -0.0002479180990739603700000, # fhc
+      -0.0000127187419158845700000, #sbp
+      -0.0000932996423232728880000 # townsend
+                   )  
   ), 
   intercept = 0, 
   mapping = "function(x){ sapply(x, function(x){
-baseline <- 0.03005139
-baseline*exp(x)
+  (1 - (0.988876402378082)^exp(x) )
 })}", 
   populationSettings = PatientLevelPrediction::createStudyPopulationSettings(
     requireTimeAtRisk = FALSE, 
@@ -77,30 +202,94 @@ baseline*exp(x)
     riskWindowEnd = 3650, 
     endAnchor = 'cohort start'
   ),
-  covariateSettings = list(
-    # creates a Chol/HDL ratio covariate with id 1466
+  
+  covariateSettings <- list(
+    
+    FeatureExtraction::createCovariateSettings(
+      useDemographicsAge = TRUE, 
+      useDemographicsEthnicity = TRUE,
+      useDemographicsRace = TRUE
+    ),
+    
+    # creates a Chol/HDL ratio covariate with id 1466/1486/1496
+    
     QRISKvalidation::createMeasurementRatioCovariateSettings(
       covariateName = 'Chol/HDL ratio', 
       conceptSet1 = c(4260765), # first measurement
       conceptSet2 = c(4042059,4076704), # second measurement 
       unitSet1 = NULL, # may need to edit this if units can be different across network
       unitSet2 = NULL, # may need to edit this if units can be different across network
-      startDay = -365*3, # last three years - TODO edit this
+      startDay = -365*1, # last year
       endDay = 0, 
-      centeringMap = function(covariates){
-        covariates$covariateValue <- sapply(covariates$covariateValue, function(y){y - 4})
-        return(covariates)
-      }, 
       minVal1 = 0,
       maxVal1 = 1000, 
       minVal2 =  0,
-      maxVal2 = 250, 
+      maxVal2 = 250,
       aggregateMethod = 'recent',
-      covariateId = 1486, 
-      analysisId = 486
+      covariateId = 1466, 
+      analysisId = 466
     ),
     
-    # family history of CVD covariate 18821668
+    QRISKvalidation::createMeasurementCovariateSettings(
+      covariateName = 'BMI', 
+      conceptSet = c(3038553),
+      unitSet = NULL, 
+      startDay = -365*5, # last 5 years
+      endDay = 0, 
+      minVal = 5,
+      maxVal = 250, 
+      aggregateMethod = 'recent',
+      covariateId = 2496, 
+      analysisId = 496
+    ),
+    
+    # Systolic blood pressure covariateId 3466
+    QRISKvalidation::createMeasurementCovariateSettings(
+      covariateName = 'Systolic Blood Pressure', 
+      conceptSet = c(3004249),
+      unitSet = NULL, 
+      startDay = -365*1, # last 1 years - check how long to look back
+      endDay = 0, 
+      minVal = 5,
+      maxVal = 250, 
+      aggregateMethod = 'recent',
+      covariateId = 3466, 
+      analysisId = 466
+    ),
+    
+    
+    QRISKvalidation::createMeasurementCovariateSettings(
+      covariateName = 'Systolic Blood Pressure stdev 5-years', 
+      conceptSet = c(3004249),
+      unitSet = NULL, 
+      startDay = -365*5, # last 3 years - check how long to look back
+      endDay = 0, 
+      minVal = 5,
+      maxVal = 250, 
+      aggregateMethod = 'stdev',
+      covariateId = 3495, 
+      analysisId = 495
+    ),
+    
+    # townsend measurement covariate 4466 - does not exist
+    QRISKvalidation::createMeasurementCovariateSettings(
+      covariateName = 'Townsend', 
+      conceptSet = c(715996),
+      unitSet = NULL, 
+      startDay = -9999, 
+      endDay = 0, 
+      minVal = NULL,
+      maxVal = NULL, 
+      aggregateMethod = 'recent',
+      covariateId = 4466, 
+      analysisId = 466
+    ),
+    
+    
+    # binary
+    #============
+    
+    # family history of CVD covariate 18821668 - QR1/2
     FeatureExtraction::createCohortBasedCovariateSettings(
       analysisId = 668, 
       covariateCohortDatabaseSchema = cohortDatabaseSchema,
@@ -112,60 +301,6 @@ baseline*exp(x)
       valueType = 'binary', 
       startDay = -9999, 
       endDay = 0
-    ),
-    
-    # townsend measurement covariate 4466
-    QRISKvalidation::createMeasurementCovariateSettings(
-      covariateName = 'Townsend', 
-      conceptSet = c(715996),
-      unitSet = NULL, 
-      startDay = -9999, 
-      endDay = 0, 
-      scaleMap = function(covariates){
-        covariates$valueAsNumber <- sapply(covariates$valueAsNumber, function(y){y - 0})
-        return(covariates)
-      }, 
-      minVal = NULL,
-      maxVal = NULL, 
-      aggregateMethod = 'recent',
-      covariateId = 4466, 
-      analysisId = 466
-    ),
-    
-    # Systolic blood pressure covariateId 3466
-    QRISKvalidation::createMeasurementCovariateSettings(
-      covariateName = 'Systolic Blood Pressure', 
-      conceptSet = c(3004249),
-      unitSet = NULL, 
-      startDay = -365, 
-      endDay = 0, 
-      scaleMap = function(covariates){
-        covariates$valueAsNumber <- sapply(covariates$valueAsNumber, function(y){y - 132.6})
-        return(covariates)
-      }, 
-      minVal = 5,
-      maxVal = 250, 
-      aggregateMethod = 'recent',
-      covariateId = 3466, 
-      analysisId = 466
-    ),
-    
-    # Standard dev of Systolic blood pressure covariateId 3466
-    QRISKvalidation::createMeasurementCovariateSettings(
-      covariateName = 'Standard deviation of Systolic Blood Pressure', 
-      conceptSet = c(3004249),
-      unitSet = NULL, 
-      startDay = -365*5, 
-      endDay = 0, 
-      scaleMap = function(covariates){
-        covariates$valueAsNumber <- sapply(covariates$valueAsNumber, function(y){y - 132.6})
-        return(covariates)
-      }, 
-      minVal = 5,
-      maxVal = 250, 
-      aggregateMethod = 'stdev',
-      covariateId = 3467, 
-      analysisId = 467
     ),
     
     # treatment for blood pressure 19280688
@@ -181,42 +316,30 @@ baseline*exp(x)
       startDay = -30, 
       endDay = 0
     ),
-
-    # type 2 diabetes 18815668
-    # rheumatoid arthritis 18838668
-    # atrial fibrillation 18841668
-    # renal disease 21347668
-    # Type I diabetes 18820668
-    # Migraine 19379668
-    # Corticosteroid use 21371668
-    # SLE 19164668
-    # Atypical antipsychotic use 21372668
-    # Severe mental illness 21294668
+    
+    
+    # QRISK 2 extras
+    #------------
+    # type 2 diabetes 18815698
+    # rheumatoid arthritis 18838698
+    # atrial fibrillation 18841698
+    # renal disease 21347698
     FeatureExtraction::createCohortBasedCovariateSettings(
-      analysisId = 668, 
+      analysisId = 698, 
       covariateCohortDatabaseSchema = cohortDatabaseSchema,
       covariateCohortTable = cohortTableName, 
       covariateCohorts = data.frame(
-        cohortId = c(18815, 18838,
-                     18841, 21347,
-                     18820, 19379,
-                     21371, 19164,
-                     21372, 21294
-                     ),
-        cohortName = c('Type 2 diabetes','Rheumatoid arthritis',
-                       'Atrial fibrillation', 'Renal disease',
-                       'Type I Diabetes', 'Migraine',
-                       'Corticosteroids', 'SLE',
-                       'Atypical antipsychotic use',
-                       'Severe mental illness'
-                       )
+        cohortId = c(18815,18838, 18841, 21347), 
+        cohortName = c('Type 2 diabetes',
+                       'Rheumatoid arthritis',
+                       'Atrial fibrillation',
+                       'Renal disease')
       ), 
       valueType = 'binary', 
       startDay = -9999, 
       endDay = 0
     ),
     
-  
     # white or not recorded 22466652
     # indian 21377652
     # Pakistani 21378652
@@ -248,7 +371,36 @@ baseline*exp(x)
       endDay = 0
     ),
     
-
+    
+    # QRISK 3 extras
+    # Type I diabetes 18820668
+    # Migraine 19379668
+    # Corticosteroid use 21371668
+    # SLE 19164668
+    # Atypical antipsychotic use 21372668
+    # Severe mental illness 21294668
+    # Erectile dysfunction or treatment 19165668
+    FeatureExtraction::createCohortBasedCovariateSettings(
+      analysisId = 668, 
+      covariateCohortDatabaseSchema = cohortDatabaseSchema,
+      covariateCohortTable = cohortTableName, 
+      covariateCohorts = data.frame(
+        cohortId = c(18820, 19379,
+                     21371, 19164,
+                     21372, 21294,
+                     19165), 
+        cohortName = c('Type I Diabetes', 'Migraine',
+                       'Corticosteroids', 'SLE',
+                       'Atypical antipsychotic use',
+                       'Severe mental illness',
+                       'Erectile dysfunction'
+        )
+      ), 
+      valueType = 'binary', 
+      startDay = -9999, 
+      endDay = 0
+    ),
+    
     # Non smoker 21287655
     # Former smoker 21288655
     # Light smoker 21289655
@@ -270,7 +422,102 @@ baseline*exp(x)
       endDay = 0
     )
     
-  )                 
+  ), 
+  
+  featureEngineering = list(
+    
+    # create the age 1 and 2
+    QRISKvalidation::createMeasurementFe(
+      covariateId = 1002, 
+      analysisId = 20, 
+      mappings = list(
+        function(x){sapply(x, function(x) (x/10)^(-1))},
+        function(x){sapply(x, function(x) (x/10)^3)}
+      )
+        ),
+    
+    # create the bmi 1 and 2
+    QRISKvalidation::createMeasurementFe(
+      covariateId = 2496, 
+      analysisId = 21, 
+      mappings = list(
+        function(x){sapply(x, function(x) (x/10)^(-2))},
+        function(x){sapply(x, function(x) ((x/10)^(-2))*log(x/10))}
+      )
+    ),
+    
+    # center all the non-binary
+    QRISKvalidation::createCenteringFe(
+      centers = data.frame(
+        covariateId = c(
+          1020, 2020, 
+          1021, 2021,
+          1466, 3466,
+          3495,
+          4466
+        ),
+        centerValue = c(
+          0.234766781330109, 77.284080505371094,
+          0.149176135659218, 0.141913309693336,
+          4.300998687744141, 128.571578979492190,
+          8.756621360778809,
+          0.526304900646210
+        )
+      )),
+    
+    # interactions
+    
+    # interaction with age 1
+    QRISKvalidation::createInteractionFe(
+      interactionCovariateId = 1020, 
+      covariateIdsOfInterest = c(
+        21288655, # Former smoker
+        21289655, # Light smoker
+        21290655, # Moderate smoker
+        21291655, # Heavy smoker 
+        18841668, # Atrial fibrillation
+        21372668, # Atypical antipsychotic use
+        21371668, # Corticosteroid use
+        19379668, # Migraine
+        21347668, # Renal disease
+        19280688, # TODO- Treated hypertension
+        18820668, # Type I Diabetes
+        18815668, # Type II Diabetes
+        1021,  # bmi 1
+        2021,  # bmi 2
+        18821668,
+        3466,
+        4466
+      ), 
+      analysisId = 123
+  ),
+  
+  # interaction with age 2
+  QRISKvalidation::createInteractionFe(
+    interactionCovariateId = 2020, 
+    covariateIdsOfInterest = c(
+      21288655, # Former smoker
+      21289655, # Light smoker
+      21290655, # Moderate smoker
+      21291655, # Heavy smoker 
+      18841668, # Atrial fibrillation
+      21372668, # Atypical antipsychotic use
+      21371668, # Corticosteroid use
+      19379668, # Migraine
+      21347668, # Renal disease
+      19280688, # TODO- Treated hypertension
+      18820668, # Type I Diabetes
+      18815668, # Type II Diabetes
+      1021,  # bmi 1
+      2021,  # bmi 2
+      18821668,
+      3466,
+      4466
+    ), 
+    analysisId = 124
+  )
+  )
+    
 )
 
 return(plpModelQRISK3_female_OG_10)
