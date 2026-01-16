cd7f842cf4a7947929db04cb6e425fd3c9dfa9b3
asaelmans <156896035+asaelmans@users.noreply.github.com>
2026-01-16 17:14:41 +0100

    Update and rename QRisk2b_male.R to QRisk2B_male.R

--- R/QRisk2B_male.R
+++ R/QRisk2B_male.R
@@ -11,7 +11,7 @@
 #' An plpModel
 #' 
 #' @export
-createQRISK2MaleOG10 <- function(
+createQRISK2BMaleOG10 <- function(
     cohortDatabaseSchema,
     cohortTableName = 'qrisk_cprd',
     sampleSize = 10000
@@ -19,7 +19,7 @@ createQRISK2MaleOG10 <- function(
   
   # Male qrisk2 2017
     
-plpModelQRISK2_male_OG_10 <- PatientLevelPrediction::createGlmModel(
+plpModelQRISK2B_male_OG_10 <- PatientLevelPrediction::createGlmModel(
   targetId = 3, # the first cohort I create manually
   outcomeId = 21397, 
   restrictPlpDataSettings = PatientLevelPrediction::createRestrictPlpDataSettings(
@@ -443,6 +443,6 @@ plpModelQRISK2_male_OG_10 <- PatientLevelPrediction::createGlmModel(
     
 )
 
-return(plpModelQRISK2_male_OG_10)
+return(plpModelQRISK2B_male_OG_10)
 }
 
