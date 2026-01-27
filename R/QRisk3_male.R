e9a62c7d8370c7215a040548296dbb0f82cb1e58
asaelmans <156896035+asaelmans@users.noreply.github.com>
2026-01-27 11:35:25 +0100

    Update QRisk3_male.R

--- R/QRisk3_male.R
+++ R/QRisk3_male.R
@@ -451,6 +451,8 @@ plpModelQRISK3_male_OG_10 <- PatientLevelPrediction::createGlmModel(
         function(x){sapply(x, function(x) ((x/10)^(-2))*log(x/10))}
       )
     ),
+    QRISKvalidation::createPreferValueAndFillFe()
+   ,
     
     # center all the non-binary
     QRISKvalidation::createCenteringFe(
