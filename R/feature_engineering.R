73b973b9b2bdea8bdb5cedbafd252963dfbf982c
asaelmans <156896035+asaelmans@users.noreply.github.com>
2026-02-17 10:22:37 +0100

    Update feature_engineering.R

--- R/feature_engineering.R
+++ R/feature_engineering.R
@@ -340,6 +340,27 @@ convertMeasurementFunc <- function(trainData,analysisId,covariateId,mappings){
   cData <- trainData$covariateData$covariates %>%
     dplyr::filter(.data$covariateId == !!covariateId) %>%
     dplyr::collect()
+    
+  existing <- trainData$covariateData$analysisRef %>%
+  dplyr::filter(.data$analysisId == !!analysisId) %>%
+  dplyr::tally() %>%
+  dplyr::collect()
+
+  if (nrow(existing) == 0 || is.na(existing$n) || existing$n == 0) {
+  Andromeda::appendToTable(
+    tbl = trainData$covariateData$analysisRef,
+    data = data.frame(
+      analysisId = as.integer(analysisId),
+      analysisName = paste0('Measurement mapping ', covariateId),
+      domainId = 'measurement mapping',
+      startDay = 0,
+      endDay = 0,
+      isBinary = 'N',
+      missingMeansZero = 'Y',
+      stringsAsFactors = FALSE
+    )
+  )
+} 
   
   print(paste0('applying mapping for ', nrow(cData), ' people'))
   
